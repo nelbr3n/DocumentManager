36c621101ff1b31cd145258fc235c0147be1d7ad
Cefeboru <cefeboru@gmail.com>
2017-09-11 22:59:05 -0600

    Se agregaron validaciones para los modelos Se cambio en la tabla statuses la columna "description" a "title" Fix para Firefox en los botones "Crear/Nuevo/Nueva"

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
     has_and_belongs_to_many :documents
+    validates :tagName, presence: true, length: { maximum: 100 }
+    validates :department_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908035555) do
+ActiveRecord::Schema.define(version: 20170912040033) do
 
   create_table "archives", force: :cascade do |t|
     t.integer  "document_id"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20170908035555) do
   end
 
   create_table "statuses", force: :cascade do |t|
-    t.string   "description"
+    t.string   "title"
     t.integer  "department_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
