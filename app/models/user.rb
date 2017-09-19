b48d8daf12e5460af4d3ac5ca9e93ceeabb939bc
edwinnuniez123 <edwin_estuadinte@yahoo.com>
2017-09-18 19:15:51 -0600

    prueba

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :department
-  validates :department_id, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :department_id, :presence => { message: "Tiene que escribir al menos 5 caracteres" }
+  validates :username, :presence => { message: "Tiene que ingresar un usuario" }
+  validates :email, :presence => { message: "Debe haber un @" }
   has_many :sentDocuments, :class_name => 'Document', :foreign_key => 'sender_id'
   has_many :receivedDocuments, :class_name => 'Document', :foreign_key => 'receiver_id'
   has_many :createdDocuments, :class_name => 'Document', :foreign_key => 'creator_id'
